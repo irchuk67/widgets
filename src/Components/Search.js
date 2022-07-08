5eb255ff98fc2f1f58581f2445e2a92a676cd3b5
irchuk67 <foxmaster110518@gmail.com>
2022-07-08 10:09:15 +0300

    Upload current project version

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,11 +1,42 @@
-import React, {useState} from "react";
+import React, {useEffect, useState } from "react";
+import axios from 'axios';
 
 let Search = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
 
-    const onTermChange = (event) => {
-        setTerm(event.target.value);
-    }
+    useEffect( () => {
+        let search = async () => {
+          const {data} = await axios.get(`https://en.wikipedia.org/w/api.php`,{
+              params:{
+                  action: 'query',
+                  list: 'search',
+                  format: 'json',
+                  origin: '*',
+                  srsearch: term,
+              }
+          })
+            setResults(data.query.search);
+        };
+        if (term){
+            search();
+        }
+
+    }, [term]);
+
+    let resultList = results.map((item) => {
+        return(
+            <div className={'item'} key={item.pageid}>
+                <div className="right floated content">
+                    <a href={`https://en.wikipedia.org?curid=${item.pageid}`} className="ui button">Go</a>
+                </div>
+                <div className="content">
+                    <div className="header">{item.title}</div>
+                    <span dangerouslySetInnerHTML={{__html: item.snippet}}></span>
+                </div>
+            </div>
+        )
+    })
     
     return(
         <div>
@@ -15,11 +46,13 @@ let Search = () => {
                     <input className={'input'}
                            type="text"
                            value={term}
-                           onChange={(event)=>onTermChange(event)}/>
+                           onChange={(event) => setTerm(event.target.value)}/>
 
                 </div>
             </div>
-
+            <div className="ui celled list">
+                {resultList}
+            </div>
         </div>
     )
 }
